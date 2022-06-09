a5bf946e60762c1102b08ef37a2151cdacbca7e2
ElizPN <pankevych.liza@gmail.com>
2022-06-09 20:12:29 +0200

    test react component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data && <div> data</div>}
+      <h1> Hello world</h1>
+      <button>click me</button>
+      <input type='text' placeholder='input value......'></input>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test App", () => {
+  test("renders learn react link", () => {
+    render(<App />);
+    const helloWorld = screen.getByText(/hello world/i);
+    const btn = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/input value/i);
+
+    expect(helloWorld).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+  test("renders learn react link", () => {
+    render(<App />);
+    // const helloWorld = screen.queryByText(/kjkjkk/i);
+    // expect(helloWorld).toBeNull;
+    const helloWorld = screen.findByText(/data/i);
+    expect(helloWorld).toBeNull;
+  });
 });
